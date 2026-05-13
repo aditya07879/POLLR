3e9d5f14c3296dc25adecd939a4933805848486c
ADITYA TOMAR <aditya07879@gmail.com>
2026-05-14 01:33:59 +0530

    add base

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://pollr-tcvr.onrender.com/api",
 });
 
 // Voter token for duplicate vote prevention (no login required)
